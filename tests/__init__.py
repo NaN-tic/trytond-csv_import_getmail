613d14f71170dd8c8f0f1c894e7e5fcfe62667ef
jmartin <jmartin@zikzakmedia.com>
2013-09-18 08:51:19 +0200

    Fixed the name of the module.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
-# This file is part of csv_import_fetch_mail module for Tryton.
+# This file is part of csv_import_getmail module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from .test_csv_import_fetch_mail import suite
+from .test_csv_import_getmail import suite
