7a1e48dbcf6a0ca7c286f749ad41b35f6ae95647
resteve <resteve@zikzakmedia.com>
2013-10-02 19:00:50 +0200

    pyflakes

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 #The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from setuptools import setup
-from setuptools import setup
 import re
 import os
 import ConfigParser
