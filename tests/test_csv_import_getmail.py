f5953f09e272d7320b99c72061711e6d1c83b7d4
resteve <resteve@zikzakmedia.com>
2015-12-01 11:06:49 +0100

    Tests

--- tests/test_csv_import_getmail.py
+++ tests/test_csv_import_getmail.py
@@ -1,38 +1,18 @@
-# This file is part of csv_import_getmail module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
-import os
-import sys
-import trytond.tests.test_tryton
+# This file is part of the csv_import_getmail module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CSVImportGetMailTestCase(unittest.TestCase):
-    '''
-    Test CSV Import GetMail module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('csv_import_getmail')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class CsvImportGetmailTestCase(ModuleTestCase):
+    'Test Csv Import Getmail module'
+    module = 'csv_import_getmail'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        CSVImportGetMailTestCase))
+        CsvImportGetmailTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
